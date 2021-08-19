50236843a51583af5516f0cd7c5fabccdd68ec20
Rhandom1 <73659834+Rhandom1@users.noreply.github.com>
2021-08-19 18:58:09 -0500

    test files built
    toBe is showing as undefined?

---
+++ Test/Intern.test.js
@@ -0,0 +1,37 @@
+const Employee = require("../lib/Employee");
+
+test("This is a test for setting the intern name through the class.", () => {
+    const name = "Shawn";
+    const emp = new Employee(name);
+
+    expect(emp.name).toBe(name)
+})
+//Have to keep parameters in order or test will move the variable into the first position
+test("This is a test for setting the intern id through the class.", () => {
+    const name = "Shawn";
+    const id = 546
+    const emp = new Employee(name, id);
+
+    expect(emp.id).toBe(id)
+
+})
+
+test("This is a test for setting the intern email through the class.", () => {
+    const name = "Shawn";
+    const id = 546
+    const email = "rhandomone@gmail.com";
+    const emp = new Employee(name, id, email);
+
+    expect(emp.email).toBe(email)
+
+})
+
+test("This is a test for setting the intern school through the class.", () => {
+    const name = "Shawn";
+    const id = 546
+    const email = "rhandomone@gmail.com";
+    const school = "UofMn";
+    const emp = new Employee(name, id, email, school);
+
+    expect(emp.school).toBe(school)
+})
---
+++ Test/Manager.test.js
@@ -0,0 +1,37 @@
+const Employee = require("../lib/Employee");
+
+test("This is a test for setting the manager name through the class.", () => {
+    const name = "Shawn";
+    const emp = new Employee(name);
+
+    expect(emp.name).toBe(name)
+})
+//Have to keep parameters in order or test will move the variable into the first position
+test("This is a test for setting the manager id through the class.", () => {
+    const name = "Shawn";
+    const id = 546
+    const emp = new Employee(name, id);
+
+    expect(emp.id).toBe(id)
+
+})
+
+test("This is a test for setting the manager email through the class.", () => {
+    const name = "Shawn";
+    const id = 546
+    const email = "rhandomone@gmail.com";
+    const emp = new Employee(name, id, email);
+
+    expect(emp.email).toBe(email)
+
+})
+
+test("This is a test for setting the manager office number through the class.", () => {
+    const name = "Shawn";
+    const id = 546
+    const email = "rhandomone@gmail.com";
+    const officeNumber = 201;
+    const emp = new Employee(name, id, email, officeNumber);
+
+    expect(emp.officeNumber).toBe(officeNumber)
+})
